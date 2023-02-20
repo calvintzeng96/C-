b78b476fe7c5d15497152b9dc77918381489e0d5
calvintzeng96 <calvintzeng96@hotmail.com>
2023-02-19 21:17:11 -0500

    c# basics d1 end

--- Program.cs
+++ Program.cs
@@ -20,4 +20,39 @@
 // simple programs you can directly start writing, but more complicated ones, one should still write out classes and methods and such...
 
 //---------------------------------------------------------------------------
-// variables:
+// assigning variables:
+// // can add const before variable type
+// const int a = 1;
+// const double b = 12.3456;
+// const char c = 'Z';
+// const string d = "hello";
+// const bool e = true;
+// Console.WriteLine(a);
+// Console.WriteLine(b);
+// Console.WriteLine(c);
+// Console.WriteLine(d);
+// Console.WriteLine(e);
+
+//number variables: (int, double, float, decimal)
+//you can use "e+number" to move decimal places
+// "_" can be used as digit separator, but isn't necessary
+// int f = 123_456;
+// double g = 12.34;
+// float h = 12.34f;
+// decimal i = 1.234m;
+// h = 12.34e3f;
+// i = 1.234e-3m;
+// Console.WriteLine(f);
+// Console.WriteLine(g);
+// Console.WriteLine(h);
+// Console.WriteLine(i);
+
+public class MyFirstApp
+{
+    public static void Main()
+    {
+        int age = 26;
+        string name = "Calvin";
+        Console.WriteLine(name +" is " + age + " years old.");
+    }
+}
--- notes.md
+++ notes.md
@@ -2,3 +2,16 @@
 
 ### semicolin should be at end of every line
 ### console.log("msg") => Console.WriteLine("msg")
+### uses pascal case for methods
+### uses camel case for variables
+### variable char wrapped in SINGLE quotes
+### variable string wrapped in DOUBLE quotes
+### variable declaration starts with type (int, double, char, string, bool)
+### can add "const" before variable type
+### *can use var instead of const, but no type needed (weird concept...?)
+
+## number types:
+### int - integers, no need to explain
+### double - takes up to 7 digits after ".", most used data type(supposedly)
+### float - takes up to 15 digits after ".", use in situations that can take rounding errs
+### decimal - takes up to 28 digits after ".", lot slower than double/float, higher level accuracy, avoid rounding errs
